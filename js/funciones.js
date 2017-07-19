7c2bddf69cde836c5b456c0326d14a9307ad3448
elisabetfr <elisabetfr@msn.com>
2017-07-19 17:15:20 +0200

    Modificación de botones

--- js/funciones.js
+++ js/funciones.js
@@ -33,10 +33,62 @@ window.onload = function(){
        headers: {
            'Authorization': 'Bearer ' + access_token
        },
-       success: function(response) {      
+       success: function(response) {  
+
+            var cadena = "";
+            response.artists.items.forEach(function(element) {
+            var myDiv = document.createElement("div");
+            myDiv.addEventListener("click", mostrar);
+              cadena = cadena + "<p>" + element.name + "</p>"
+              if(element.images.length !== 0){
+              cadena = cadena + "<img src=" + element.images[1].url + ">";
+            }
+            myDiv.innerHTML = cadena;
+            document.getElementById('ajaxContainer').appendChild(myDiv);
+            cadena= "";
+            console.log("esi")
+            });     
+            console.log(cadena);
+       }
+    });
+  });
+  
+  $buttonAlbum.on("click", function(e) {
+
+      e.preventDefault();
+      var $inputValue = $('#query').val;  
+      /*var sArtistToFind = $inputValue.val();*/
+      //var sGetArtistsUrl = "https://api.spotify.com/v1/search?type=artist&query=" + sArtistToFind;
+
+       function getHashParams() {
+        var hashParams = {};
+        var e, r = /([^&;=]+)=?([^&;]*)/g,
+                  q = window.location.hash.substring(1);
+              while ( e = r.exec(q)) {
+                 hashParams[e[1]] = decodeURIComponent(e[2]);
+              }
+              return hashParams;
+            }
+
+      var params = getHashParams();
+
+      var access_token = params.access_token;
+
+    $.ajax({
+       url: 'https://api.spotify.com/v1/search',
+       dataType: 'json',
+       data: {
+          type: "album",
+          query : document.getElementById('example-text-input').value
+        },
+       headers: {
+           'Authorization': 'Bearer ' + access_token
+       },
+       success: function(response) { 
+       qwerty = response;  
 
             var cadena = "<ul>";
-            response.artists.items.forEach(function(element) {
+            response.albums.items.forEach(function(element) {
               cadena = cadena + "<li>" + element.name + "</li>"
               if(element.images.length !== 0){
               cadena = cadena + "<img src=" + element.images[1].url + ">";
@@ -49,6 +101,52 @@ window.onload = function(){
        }
     });
   });
+
+  function mostrar(){
+
+    e.preventDefault();
+      var $inputValue = $('#query').val;  
+      /*var sArtistToFind = $inputValue.val();*/
+      //var sGetArtistsUrl = "https://api.spotify.com/v1/search?type=artist&query=" + sArtistToFind;
+
+       function getHashParams() {
+        var hashParams = {};
+        var e, r = /([^&;=]+)=?([^&;]*)/g,
+                  q = window.location.hash.substring(1);
+              while ( e = r.exec(q)) {
+                 hashParams[e[1]] = decodeURIComponent(e[2]);
+              }
+              return hashParams;
+            }
+
+      var params = getHashParams();
+
+      var access_token = params.access_token;
+
+      $.ajax({
+       url: 'https://api.spotify.com/v1/artists/{id}/albums',
+       dataType: 'json',
+       headers: {
+           'Authorization': 'Bearer ' + access_token
+       },
+       success: function(response) { 
+       qwerty = response;  
+
+            var cadena = "<ul>";
+            response.albums.items.forEach(function(element) {
+              cadena = cadena + "<li>" + element.name + "</li>"
+              if(element.images.length !== 0){
+              cadena = cadena + "<img src=" + element.images[1].url + ">";
+            }
+            });     
+
+            cadena = cadena + "</ul>"
+            document.getElementById('ajaxContainer').innerHTML = cadena;
+            console.log(cadena);
+       }
+    });
+    console.log("sdfsdf")
+  }
 
   $buttonAlbum.on("click", function(e) {
 
@@ -82,7 +180,7 @@ window.onload = function(){
            'Authorization': 'Bearer ' + access_token
        },
        success: function(response) { 
-       qwerty = response;     
+       qwerty = response;  
 
             var cadena = "<ul>";
             response.albums.items.forEach(function(element) {
