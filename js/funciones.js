926ffa46eefa33589a7baac19ab0920e8fceef28
elisabetfr <elisabetfr@msn.com>
2017-07-19 14:38:35 +0200

    actualizado

--- js/funciones.js
+++ js/funciones.js
@@ -1,5 +1,6 @@
 window.onload = function(){
   var $button = $('#request');
+  var $buttonAlbum = $('#request-album');
 
   $button.on("click", function(e) {
 
@@ -27,18 +28,19 @@ window.onload = function(){
        dataType: 'json',
        data: {
           type: "artist",
-            query : "beyonce"
+          query : document.getElementById('example-text-input').value
         },
        headers: {
            'Authorization': 'Bearer ' + access_token
        },
        success: function(response) {      
-            qwerty = response;
-            document.getElementById('ajaxContainer').innerHTML = response;
-            console.log(response);
+
             var cadena = "<ul>";
             response.artists.items.forEach(function(element) {
               cadena = cadena + "<li>" + element.name + "</li>"
+              if(element.images.length !== 0){
+              cadena = cadena + "<img src=" + element.images[1].url + ">";
+            }
             });     
 
             cadena = cadena + "</ul>"
@@ -48,4 +50,52 @@ window.onload = function(){
     });
   });
 
+  $buttonAlbum.on("click", function(e) {
+
+      e.preventDefault();
+      var $inputValue = $('#query').val;  
+      /*var sArtistToFind = $inputValue.val();*/
+      //var sGetArtistsUrl = "https://api.spotify.com/v1/search?type=artist&query=" + sArtistToFind;
+
+       function getHashParams() {
+        var hashParams = {};
+        var e, r = /([^&;=]+)=?([^&;]*)/g,
+                  q = window.location.hash.substring(1);
+              while ( e = r.exec(q)) {
+                 hashParams[e[1]] = decodeURIComponent(e[2]);
+              }
+              return hashParams;
+            }
+
+      var params = getHashParams();
+
+      var access_token = params.access_token;
+
+    $.ajax({
+       url: 'https://api.spotify.com/v1/search',
+       dataType: 'json',
+       data: {
+          type: "album",
+          query : document.getElementById('example-text-input').value
+        },
+       headers: {
+           'Authorization': 'Bearer ' + access_token
+       },
+       success: function(response) { 
+       qwerty = response;     
+
+            var cadena = "<ul>";
+            response.albums.items.forEach(function(element) {
+              cadena = cadena + "<li>" + element.name + "</li>"
+              if(element.images.length !== 0){
+              cadena = cadena + "<img src=" + element.images[1].url + ">";
+            }
+            });     
+
+            cadena = cadena + "</ul>"
+            document.getElementById('ajaxContainer').innerHTML = cadena;
+            console.log(cadena);
+       }
+    });
+  });
 }
